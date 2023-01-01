842ddde0d1a2f488948ad05367de174be384dc02
Manish Vijay Sonawane <sonawanemanish349@gmail.com>
2023-01-01 16:15:50 +0530

    add boiler plate

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import style from "./Navbar.css";
+
+function Navbar(){
+    return <>
+    <div className="main">
+        <div>HOME</div>
+        <div>ABOUT</div>
+        <div>CONTACT</div>
+        <div>EDUCATION</div>
+        <div>RESUME</div>
+    </div>
+    </>
+}
+export default Navbar;
